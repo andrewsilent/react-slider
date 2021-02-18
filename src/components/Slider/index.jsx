214b9f32a84e951a2fb9d73698eef0836033b533
andrewsilent <silent.netix@gmail.com>
2021-02-18 20:17:54 +0200

    add controls and slide-effect

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -12,9 +12,18 @@ class Slider extends Component {
         "https://i.pinimg.com/originals/d8/25/b9/d825b96f6f6df5366e227f71679d04c7.jpg",
       ],
       descriptions: [
-        { "Some title #0": "Some description #0" },
-        { "Some title #1": "Some description #1" },
-        { "Some title #2": "Some description #2" },
+        {
+          "Some title #0":
+            "Some description #0 Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias est labore optio quos provident ex veritatis facilis sapiente dicta assumenda.",
+        },
+        {
+          "Some title #1":
+            "Some description #1 Quibusdam nobis veniam ex labore! Incidunt facilis blanditiis dolor distinctio. Odio quasi inventore culpa fugit accusamus vitae provident voluptas molestias.",
+        },
+        {
+          "Some title #2":
+            "Some description #2 Reiciendis, quae deserunt eaque qui quasi explicabo recusandae nihil labore hic repudiandae sit quaerat quam officia! Nihil veritatis ipsum aliquid.",
+        },
       ],
       currentSlide: 0,
     };
@@ -36,34 +45,41 @@ class Slider extends Component {
 
   render() {
     const { currentSlide, slides, descriptions } = this.state;
-    console.log(styles.current);
     return (
       <main className={styles.container}>
         <section className={styles.slider}>
-          <button className={styles.control} onClick={this.prevSlide}>
-            prev
-          </button>
-          <button className={styles.control} onClick={this.nextSlide}>
-            next
-          </button>
-          {slides.map((e, i) => {
-            return (
-              <div
-                key={e}
-                className={
-                  currentSlide === i
-                    ? `${styles.current} ${styles.slide}`
-                    : `${styles.slide}`
-                }
-              >
-                <Slide
-                  src={slides[i]}
-                  currentSlide={currentSlide}
-                  description={descriptions[i]}
-                />
-              </div>
-            );
-          })}
+          <div className={styles.controls}>
+            <div className={styles.controlsInner}>
+              <button
+                className={styles.control}
+                onClick={this.prevSlide}
+              ></button>
+              <button
+                className={styles.control}
+                onClick={this.nextSlide}
+              ></button>
+            </div>
+          </div>
+          <div className={styles.slides}>
+            {slides.map((e, i) => {
+              return (
+                <div
+                  key={e}
+                  className={
+                    currentSlide === i
+                      ? `${styles.current} ${styles.slide}`
+                      : `${styles.slide}`
+                  }
+                >
+                  <Slide
+                    src={slides[i]}
+                    currentSlide={currentSlide}
+                    description={descriptions[i]}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </section>
       </main>
     );
